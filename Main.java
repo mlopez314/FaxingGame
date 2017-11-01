0fe7b178a3e246b85e2d07ac544862b3d01816c2
mlopez312 <lopez.m@husky.neu.edu>
2017-11-01 10:13:10 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+import javax.swing.JFrame;
+
+import ff.Canvas;
+
+/**
+ * This is the main method that will run the game.
+ * 
+ * @author Marcos Lopez
+ */
+public class Main {
+  public static void main(String args[]) {
+    JFrame frame = new JFrame("Faxing Game v0.2.2");
+    frame.setSize(800, 800);
+    frame.setLocationRelativeTo(null);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setResizable(false);
+    
+    Canvas c = new Canvas();
+    frame.add(c);
+    
+    frame.setVisible(true);
+  }
+}
